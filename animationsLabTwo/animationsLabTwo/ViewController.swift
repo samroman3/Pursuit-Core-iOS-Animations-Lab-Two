edae876f5ece03ec7db9b0e483f96f1e202b7327
Sam <samroman3@gmail.com>
2019-10-14 16:06:04 -0400

    Adds buttons and remaining views with corresponding constraints

--- animationsLabTwo/animationsLabTwo/ViewController.swift
+++ animationsLabTwo/animationsLabTwo/ViewController.swift
@@ -38,14 +38,99 @@ class ViewController: UIViewController {
         return view
     }()
     
+    //MARK: Button Outlets
+    lazy var linear: UIButton = {
+        let button = UIButton()
+        button.setTitle("Linear", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
+    lazy var easeIn: UIButton = {
+        let button = UIButton()
+        button.setTitle("Ease In", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
+    lazy var easeOut: UIButton = {
+        let button = UIButton()
+        button.setTitle("Ease Out", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
+    lazy var easeInOut: UIButton = {
+        let button = UIButton()
+        button.setTitle("In/Out", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        
+        return button
+    }()
+    
+    lazy var animateButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Animate", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
+    lazy var resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
     
     
-    //MARK: Constraints
+    
+    
+    //MARK: Button StackView & Constraints
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [linear, easeIn, easeOut, easeInOut])
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.distribution = .fillProportionally
+        stack.spacing = 5.0
+        stack.isUserInteractionEnabled = true
+        return stack
+    }()
+    
+    private func constrainButtonStackView() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            stackView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+        ])
+    }
+    
+    private func constrainResetButton(){
+        resetButton.translatesAutoresizingMaskIntoConstraints = false
+           resetButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -170).isActive = true
+           resetButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 400).isActive = true
+        
+    }
+    
+    
+    private func constrainAnimateButton(){
+        animateButton.translatesAutoresizingMaskIntoConstraints = false
+           animateButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 160).isActive = true
+           animateButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 400).isActive = true
+        
+    }
+    
+   
+    
+    
+    //MARK: View Constraints
     private func setUpViewOneConstraints(){
            viewOne.translatesAutoresizingMaskIntoConstraints = false
            NSLayoutConstraint.activate([
             viewOneCenterYConstraint,
-            viewOne.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -150),
+            viewOne.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -170),
                viewOne.widthAnchor.constraint(equalToConstant: 50),
                viewOne.heightAnchor.constraint(equalToConstant: 50)
            ])
@@ -59,7 +144,7 @@ class ViewController: UIViewController {
               viewTwo.translatesAutoresizingMaskIntoConstraints = false
               NSLayoutConstraint.activate([
                viewTwoCenterYConstraint,
-               viewTwo.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -55),
+               viewTwo.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -75),
                   viewTwo.widthAnchor.constraint(equalToConstant: 50),
                   viewTwo.heightAnchor.constraint(equalToConstant: 50)
               ])
@@ -69,25 +154,72 @@ class ViewController: UIViewController {
            self.viewTwo.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200)
        }()
        
+    private func setUpViewThreeConstraints(){
+             viewThree.translatesAutoresizingMaskIntoConstraints = false
+             NSLayoutConstraint.activate([
+              viewThreeCenterYConstraint,
+              viewThree.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 40),
+                 viewThree.widthAnchor.constraint(equalToConstant: 50),
+                 viewThree.heightAnchor.constraint(equalToConstant: 50)
+             ])
+         }
+      
+      lazy var viewThreeCenterYConstraint: NSLayoutConstraint = {
+          self.viewThree.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200)
+      }()
+      
+      private func setUpViewFourConstraints(){
+                viewFour.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                 viewFourCenterYConstraint,
+                 viewFour.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 170),
+                    viewFour.widthAnchor.constraint(equalToConstant: 50),
+                    viewFour.heightAnchor.constraint(equalToConstant: 50)
+                ])
+            }
+         
+         lazy var viewFourCenterYConstraint: NSLayoutConstraint = {
+             self.viewFour.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200)
+         }()
+         
+    
+    
+    private func setAllConstraints(){
+        constrainButtonStackView()
+        constrainResetButton()
+        constrainAnimateButton()
+        setUpViewOneConstraints()
+        setUpViewTwoConstraints()
+        setUpViewThreeConstraints()
+        setUpViewFourConstraints()
+        
+        
+    }
     
     
     
     //MARK: View Methods
     
     
+    
+    
+    
 
     //MARK: Private Methods
     private func addSubViews(){
+        view.addSubview(stackView)
         view.addSubview(viewOne)
         view.addSubview(viewTwo)
         view.addSubview(viewThree)
         view.addSubview(viewFour)
+        view.addSubview(resetButton)
+        view.addSubview(animateButton)
+        
     }
     
     override func viewDidLoad() {
         addSubViews()
-        setUpViewOneConstraints()
-        setUpViewTwoConstraints()
+        setAllConstraints()
         view.backgroundColor = .white
         super.viewDidLoad()
         // Do any additional setup after loading the view.
